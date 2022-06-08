90ac97e02e988b490b377c3881ff87bb9088f668
Anarasty <murloc.ninja16@gmail.com>
2022-06-08 10:44:01 +0300

    redux aded

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "IPhone 13 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image: "https://res.cloudinary.com/dvew6rdcg/image/upload/v1654670279/Apple/iphone13_adjfrj.png",
+  },
+  {
+    id: 2,
+    name: "IPhone 12 Pro",
+    desc: "5.4-inch display",
+    price: 999,
+    image: "https://res.cloudinary.com/dvew6rdcg/image/upload/v1654670279/Apple/iphone12_q6s52w.png",
+  },
+  {
+    id: 2,
+    name: "IPhone 11 Pro",
+    desc: "4.2-inch display",
+    price: 999,
+    image: "https://res.cloudinary.com/dvew6rdcg/image/upload/v1654670279/Apple/iphone11_r1ofx5.png",
+  },
+];
+
+module.exports = products;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer from "./features/productsSlice";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
